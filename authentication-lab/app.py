15365b092a73d7e06ef8403eef0c894338048b86
jawadab24-meet <jawadabusalih521@gmail.com>
2023-07-23 18:29:56 +0300

    i don't know  done

--- authentication-lab/app.py
+++ authentication-lab/app.py
@@ -1,8 +1,23 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask import session as login_session
 import pyrebase
+config = {
+  apiKey: "AIzaSyDhpOFHB39VaPCWTmEW7Jyy3FoyPkmWWpc",
+  authDomain: "jawad-9fd16.firebaseapp.com",
+  projectId: "jawad-9fd16",
+  storageBucket: "jawad-9fd16.appspot.com",
+  messagingSenderId: "190357888489",
+  appId: "1:190357888489:web:50cef14729a7a77debbf78",
+  measurementId: "G-9K8NX1WR30"
+}
 
-app = Flask(__name__, template_folder='templates', static_folder='static')
+
+
+firebase = pyrebase.initialize_app(config)
+auth = firebase.auth()
+
+
+app  = Flask(__name__, template_folder='templates', static_folder='static')
 app.config['SECRET_KEY'] = 'super-secret-key'
 
 
@@ -13,6 +28,14 @@ def signin():
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
+    if request.methods == 'POT':
+     email = request.form['email']
+     password = request.form['password']
+     try: 
+        login_session['user'] = auth.creat_user__with__email_and_password(email,password)
+        return redirect(url_for('home'))
+    except:
+        error = "auth failed"
     return render_template("signup.html")
 
 
